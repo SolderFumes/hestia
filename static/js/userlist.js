b8b33231cf55d28d728d856d2e980e576c81689c
solderfumes <luka.schuller@gmail.com>
2025-07-15 14:14:42 -0700

    Added input validation

--- static/js/userlist.js
+++ static/js/userlist.js
@@ -15,21 +15,28 @@ function user_popup() {
     let uploadPicture = document.createElement('input');
     uploadPicture.type = 'file';
     uploadPicture.name = 'img';
+    uploadPicture.required = true;
     createForm.appendChild(uploadPicture);
 
     let nameInput = document.createElement('input');
     nameInput.name = 'name';
     nameInput.placeholder = 'Name...';
+    nameInput.required = true;
     createForm.appendChild(nameInput);
 
     let songInput = document.createElement('input');
     songInput.name = 'song_url';
-    songInput.placeholder = 'Song URL...';
+    songInput.type = 'url';
+    songInput.placeholder = 'https://spotify.com/example';
+    songInput.pattern = 'https://.*'
+    songInput.required = true;
     createForm.appendChild(songInput);
 
     let lightInput = document.createElement('input');
     lightInput.name = 'light_color';
+    lightInput.required = true;
     lightInput.placeholder = 'R (0-255), G (0-255), B (0-255)';
+    lightInput.pattern = '.{1,3},.{1,3},.{1,3}';
     createForm.appendChild(lightInput);
 
     let submit = document.createElement('input');
