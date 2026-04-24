1f42ccf64b0b8454fae6d3e5ac0e27f69bf67e82
solderfumes <luka.schuller@gmail.com>
2026-04-24 15:59:53 -0700

    Switched from JSON to UTF-8 text. Example uses .hst file extension but it doesn't actually matter what the extension is. .hst doesn't even exist except for some weird raw BMP format some random company made up. I can make up my own too :D

--- actions.py
+++ actions.py
@@ -15,13 +15,13 @@
 
 # We can call this function as often as we want because it's just performing file I/O. We'll call this every 5 seconds or so.
 
-import json
+import ast
 
-def get_actions(filename: str = 'actions.json'):
+def get_actions(filename: str = 'actions.hst'):
     with open(filename, 'r') as file:
-        return json.load(file)
+        return ast.literal_eval(file.read()) 
 
-def actions_pretty(filename: str = 'actions.json'):
+def actions_pretty(filename: str = 'actions.json') -> list:
     '''
     Returns a List of tuples in which the first element is the type of the first entry in the JSON file filname and the second element is the entity_id.
     '''
@@ -29,20 +29,23 @@ def actions_pretty(filename: str = 'actions.json'):
     entity_id = ''
     retlist = []
     data = get_actions()
-    for key, value in data.items():
-        entity_type = key.split('/')[1] # this should always be the type
-        if entity_type == 'media_player':
-            entity_type = 'Media Player'
-        entity_id = value['entity_id']
-        retlist.append((entity_type, entity_id))
-    print('Prettified list:',retlist)
+    for action_tuple in data:
+        for key, value in action_tuple[1].items():
+            if key == 'entity_id':
+                entity_id = value
+                entity_type = action_tuple[0].split('/')[1] # This should always be the entity type
+                retlist.append((entity_type, entity_id))
+    print('prettified:', retlist)
     return retlist
 
 
 
-# We also want to be able to write the data to the file. We'll just take the dictionary as an argument and make someone else edit the dictionary. We supply a helper function to allow them to get the dictionary though, that way they don't have to do any file openining :) yay helpful
-def write_data(data: dict, filename: str = 'actions.json'):
-    with open(filename, 'w'):
-        json.dumps(json, indent=4)
+
+def write_data(data: list, filename: str = 'actions.hst'):
+    '''
+    Writes string representation of actions list to filename provided
+    '''
+    with open(filename, 'w') as file:
+        file.write(str(data))
 
 
